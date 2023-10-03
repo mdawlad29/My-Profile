fbdb32a9b3a7b932d2af9a5f8b3aa0f648d8b3cd
mdawlad29 <mdawladbdinfo@gmail.com>
2023-10-03 12:39:48 +0600

    Tailwind config updated

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,6 +1,7 @@
 import type { Config } from 'tailwindcss'
 
 const config: Config = {
+  important:"true",
   content: [
     './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
     './src/components/**/*.{js,ts,jsx,tsx,mdx}',
@@ -12,6 +13,24 @@ const config: Config = {
         'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
         'gradient-conic':
           'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+      },
+      backgroundColor:{
+        primary:"#00A34F",
+        secondary:"#ED1C24",
+        accent:"#263170",
+        neutral:"#fff"
+      },
+      textColor:{
+        primary:"#00A34F",
+        secondary:"#ED1C24",
+        accent:"#263170",
+        neutral:"#fff"
+      },
+      borderColor:{
+        primary:"#00A34F",
+        secondary:"#ED1C24",
+        accent:"#263170",
+        neutral:"#fff"
       },
     },
   },
